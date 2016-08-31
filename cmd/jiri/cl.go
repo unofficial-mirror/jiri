202cb08769219f844b6d80aa28b9d4f2f7a60cbe
Petr Hosek <phosek@google.com>
2016-08-30 18:25:35 -0700

    Provide jiri cl patch command
    This command can be used to patch in one or more existing changes
    into the project.
    
    Change-Id: Ic2008a4fc161edc53ec6e95adda9ae8354e0f06c

--- cmd/jiri/cl.go
+++ cmd/jiri/cl.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 
 	"fuchsia.googlesource.com/jiri"
@@ -48,6 +49,8 @@ var (
 	verifyFlag            bool
 	currentProjectFlag    bool
 	cleanupMultiPartFlag  bool
+	branchFlag            string
+	deleteFlag            bool
 )
 
 // Special labels stored in the commit message.
@@ -91,6 +94,10 @@ func init() {
 	cmdCLMail.Flags.BoolVar(&verifyFlag, "verify", true, `Run pre-push git hooks.`)
 	cmdCLMail.Flags.BoolVar(&currentProjectFlag, "current-project-only", false, `Run mail in the current project only.`)
 	cmdCLMail.Flags.BoolVar(&cleanupMultiPartFlag, "clean-multipart-metadata", false, `Cleanup the metadata associated with multipart CLs pertaining the MultiPart: x/y message without mailing any CLs.`)
+	cmdCLPatch.Flags.StringVar(&branchFlag, "branch", "", "Name of the branch the patch will be applied to")
+	cmdCLPatch.Flags.BoolVar(&deleteFlag, "delete", false, "Delete the existing branch if already exists")
+	cmdCLPatch.Flags.BoolVar(&forceFlag, "force", false, "Use force when deleting the existing branch")
+	cmdCLPatch.Flags.StringVar(&hostFlag, "host", "", `Gerrit host to use.  Defaults to gerrit host specified in manifest.`)
 	cmdCLSync.Flags.StringVar(&remoteBranchFlag, "remote-branch", "master", `Name of the remote branch the CL pertains to, without the leading "origin/".`)
 }
 
@@ -140,7 +147,7 @@ func newCmdCL() *cmdline.Command {
 		Name:     "cl",
 		Short:    "Manage changelists for multiple projects",
 		Long:     "Manage changelists for multiple projects.",
-		Children: []*cmdline.Command{cmdCLCleanup, cmdCLMail, cmdCLNew, cmdCLSync},
+		Children: []*cmdline.Command{cmdCLCleanup, cmdCLMail, cmdCLNew, cmdCLPatch, cmdCLSync},
 	}
 }
 
@@ -1395,6 +1402,155 @@ func newCL(jirix *jiri.X, args []string) error {
 
 	cleanup = false
 	return nil
+}
+
+// cmdCLPatch represents the "jiri cl patch" command.
+var cmdCLPatch = &cmdline.Command{
+	Runner: jiri.RunnerFunc(runCLPatch),
+	Name:   "patch",
+	Short:  "Patch in the existing change",
+	Long: `
+Command "patch" applies the existing changelist to the current project. The
+change can be identified either using change ID, in which case the latest
+patchset will be used, or the the full reference.
+
+A new branch will be created to apply the patch to. The default name of this
+branch is "change/<changeset>/<patchset>", but this can be overriden using the
+-branch flag. The command will fail if the branch already exists. The -delete
+flag will delete the branch if already exists. Use the -force flag to force
+deleting the branch even if it contains unmerged changes).
+`,
+	ArgsName: "<change>",
+	ArgsLong: "<change> is a change ID or a full reference.",
+}
+
+// patchProject changes directory into the project directory, checks out the given
+// change, then cds back to the original directory.
+func patchProject(jirix *jiri.X, project project.Project, ref string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(cwd)
+
+	if err = os.Chdir(project.Path); err != nil {
+		return err
+	}
+
+	var branch string
+	if branchFlag != "" {
+		branch = branchFlag
+	} else {
+		cl, ps, err := gerrit.ParseRefString(ref)
+		if err != nil {
+			return err
+		}
+		branch = fmt.Sprintf("change/%v/%v", cl, ps)
+	}
+
+	git := gitutil.New(jirix.NewSeq())
+	if git.BranchExists(branch) {
+		if deleteFlag {
+			if err := git.CheckoutBranch("origin/master"); err != nil {
+				return err
+			}
+			if err := git.DeleteBranch(branch, gitutil.ForceOpt(forceFlag)); err != nil {
+				return err
+			}
+		} else {
+			return fmt.Errorf("branch %v already exists in project %q", branch, project.Name)
+		}
+	}
+	if err := git.CreateAndCheckoutBranch(branch); err != nil {
+		return err
+	}
+	if err := git.Pull(project.Remote, ref); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func patchCL(jirix *jiri.X, arg string) (e error) {
+	cl, ps, err := gerrit.ParseRefString(arg)
+	if err != nil {
+		cl, err = strconv.Atoi(arg)
+		if err != nil {
+			return fmt.Errorf("invalid argument: %v", arg)
+		}
+	}
+
+	if p, err := currentProject(jirix); err == nil {
+		if ps != -1 {
+			if err := patchProject(jirix, p, arg); err != nil {
+				return err
+			}
+		}
+
+		host := hostFlag
+		if host == "" {
+			if p.GerritHost == "" {
+				return fmt.Errorf("no Gerrit host; use the '--host' flag, or add a 'gerrithost' attribute for project %q", p.Name)
+			}
+			host = p.GerritHost
+		}
+		hostUrl, err := url.Parse(host)
+		if err != nil {
+			return fmt.Errorf("invalid Gerrit host %q: %v", host, err)
+		}
+		g := jirix.Gerrit(hostUrl)
+
+		c, err := g.GetChange(cl)
+		if err != nil {
+			return err
+		}
+		if err := patchProject(jirix, p, c.Reference()); err != nil {
+			return err
+		}
+	} else {
+		host := hostFlag
+		if host == "" {
+			return fmt.Errorf("no Gerrit host; use the '--host' flag")
+		}
+		hostUrl, err := url.Parse(host)
+		if err != nil {
+			return fmt.Errorf("invalid Gerrit host %q: %v", host, err)
+		}
+		g := jirix.Gerrit(hostUrl)
+
+		c, err := g.GetChange(cl)
+		if err != nil {
+			return err
+		}
+		var ref string
+		if ps != -1 {
+			ref = arg
+		} else {
+			ref = c.Reference()
+		}
+
+		projects, _, err := project.LoadManifest(jirix)
+		if err != nil {
+			return err
+		}
+
+		for _, p := range projects {
+			if strings.HasSuffix(p.Remote, c.Project) {
+				if err := patchProject(jirix, p, ref); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func runCLPatch(jirix *jiri.X, args []string) error {
+	if expected, got := 1, len(args); expected != got {
+		return jirix.UsageErrorf("unexpected number of arguments: expected %v, got %v", expected, got)
+	}
+	return patchCL(jirix, args[0])
 }
 
 // cmdCLSync represents the "jiri cl sync" command.
