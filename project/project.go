bdb3a10f925fa3bfb351f1fe6dd8eb87ef8a98bc
Ankur Mittal <anmittal@google.com>
2016-09-30 11:19:05 -0700

    Loader should load manifest file only once
    This will help us to detect two projects, hooks and other stuff
    with same ID.
    
    Change-Id: I95841e26fda50d63af7e8f405a80cd04f89418a9

--- project/project.go
+++ project/project.go
@@ -1247,6 +1247,7 @@ func newManifestLoader(localProjects Projects, update bool) *loader {
 		Hooks:         make(Hooks),
 		localProjects: localProjects,
 		update:        update,
+		manifests:     make(map[string]bool),
 	}
 }
 
@@ -1257,6 +1258,7 @@ type loader struct {
 	localProjects Projects
 	update        bool
 	cycleStack    []cycleInfo
+	manifests     map[string]bool
 }
 
 type cycleInfo struct {
@@ -1326,6 +1328,10 @@ func (ld *loader) Load(jirix *jiri.X, root, file, cycleKey string) error {
 }
 
 func (ld *loader) load(jirix *jiri.X, root, file string) error {
+	if ld.manifests[file] {
+		return nil
+	}
+	ld.manifests[file] = true
 	m, err := ManifestFromFile(jirix, file)
 	if err != nil {
 		return err
