a45ef20f6e879cee49efb5ae9c9b8313ba716840
Haowei Wu <haowei@google.com>
2019-01-25 11:34:48 -0800

    [bootstrap] Add bootstrap command to jiri
    This patch add 'jiri bootsrap' command to jiri to allow user to
    fetch cipd without cloning any fuchsia repos.
    
    Change-Id: Iab1321abe3937f7f2faca444d41b93716fc5a9ad

---
+++ cmd/jiri/bootstrap.go
@@ -0,0 +1,47 @@
+// Copyright 2018 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"fuchsia.googlesource.com/jiri"
+	"fuchsia.googlesource.com/jiri/cipd"
+	"fuchsia.googlesource.com/jiri/cmdline"
+)
+
+var cmdBootstrap = &cmdline.Command{
+	Runner: jiri.RunnerFunc(runBootstrap),
+	Name:   "bootstrap",
+	Short:  "Bootstrap essential packages",
+	Long: `
+Bootstrap essential packages such as cipd.
+`,
+	ArgsName: "<package ...>",
+	ArgsLong: "<package ...> is a list of packages that can be bootstraped by jiri. If the list is empty, jiri will list supported packages.",
+}
+
+func runBootstrap(jirix *jiri.X, args []string) error {
+	if len(args) == 0 {
+		// Currently it only supports cipd. We may add more packages from buildtools in the future.
+		fmt.Printf("Supported package(s):\n\tcipd\n")
+		return nil
+	}
+	for _, v := range args {
+		switch strings.ToLower(v) {
+		case "cipd":
+			cipdPath, err := cipd.Bootstrap()
+			if err != nil {
+				return err
+			}
+			fmt.Printf("cipd boostraped to path:%q\n", cipdPath)
+
+		default:
+			return fmt.Errorf("unsupported package %q", v)
+		}
+	}
+	return nil
+}
